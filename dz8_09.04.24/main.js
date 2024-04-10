6240dd721bffbf430906758eefae82220dfdcbc0
Anzhelika <ztlav2023@gmail.com>
2024-04-10 21:22:50 +0200

    add new

---
+++ dz8_09.04.24/main.js
@@ -0,0 +1,29 @@
+let a = 25;
+
+let b = 'javascript';
+
+let c = false;
+
+console.log(a, b, c);
+
+let d = 'python';
+console.log(d);
+
+let f = d;
+console.log(f);
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => {
+  console.log(number);
+});
+
+
+
+const numbers2 = [1, 2, 3, 4, 5];
+
+const reversedNumbers = numbers.reverse();
+
+console.log(reversedNumbers);
